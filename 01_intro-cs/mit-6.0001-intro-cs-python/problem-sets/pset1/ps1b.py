bd2d764ea9462793d4ce7a9ad92f3a30d47a4f46
farahcodes <farah.fournat@gmail.com>
2025-05-30 21:54:37 +0200

    Update ps1b.py to include user input prompts

--- 01_intro-cs/mit-6.0001-intro-cs-python/problem-sets/pset1/ps1b.py
+++ 01_intro-cs/mit-6.0001-intro-cs-python/problem-sets/pset1/ps1b.py
@@ -6,7 +6,10 @@
 ##########################################################################################
 ## Get user input for yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise below ##
 ##########################################################################################
-
+yearly_salary = float(input("Enter your yearly salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
@@ -14,6 +17,6 @@
 
 
 ###############################################################################################
-## Determine how many months it would take to get the down payment for your dream home below ## 
+## Determine how many months it would take to get the down payment for your dream home below ##
 ###############################################################################################
 
