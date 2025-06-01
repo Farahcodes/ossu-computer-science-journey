a70967b55d3341c16d7b3dbd2f1699cddacb94eb
farahcodes <farah.fournat@gmail.com>
2025-06-02 00:41:27 +0200

    Add finger exercise for Lecture 4

---
+++ 01_intro-cs/mit-6.0001-intro-cs-python/finger-exercises/lecture04_finger_ex.py
@@ -0,0 +1,12 @@
+
+# Finger Exercise Lecture 4
+# Assume you are given a positive integer variable named N. Write a piece of Python code that finds the cube root of N. The code prints the cube root if N is a perfect cube or it prints error if N is not a perfect cube. Hint: use a loop that increments a counter—you decide when the counter should stop.
+
+N = 27
+
+for i in range(N):
+    if i**3 == N:
+        print(i)
+        break
+else:
+    print("error")
